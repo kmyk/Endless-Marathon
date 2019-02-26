d30bcd8eb9911dc437a839326fb45669187e86c0
kosakkun <kosakkun0918@gmail.com>
2019-02-26 21:53:01 +0900

    code-testをflaskで実装した．

--- run.py
+++ run.py
@@ -1,18 +1,40 @@
+import os
+import subprocess
+
 from flask import *
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-	return render_template('index.html')
+	return render_template("index.html")
 
-@app.route('/code_test', methods=["GET", "POST"])
+@app.route("/code_test", methods=["GET", "POST"])
 def code_test():
     if request.method == "GET":
-        return render_template('code_test.html')
+        return render_template("code_test.html", code="", stdin="", stdout="", stderr="")
     else:
-        return render_template('code_test.html')
-    
-if __name__ == '__main__':
-	app.run()
+        code  = request.form["code"] if "code" in request.form else ""
+        stdin = request.form["stdin"] if "stdin" in request.form else ""
+        lang  = request.form["lang-sel"]
+        
+        if not os.path.exists("execute"):
+            os.makedirs("execute")
+        with open("execute/input.txt", "w") as file:
+            file.write(stdin)
+        
+        stdout = ""
+        stderr = ""
+        if lang == "cpp":
+            with open("execute/a.cpp", "w") as file:
+                file.write(code)
+            cmd = "g++ execute/a.cpp -o execute/a.out -std=c++11 && time execute/a.out < execute/input.txt"
+            proc = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout = proc.stdout.decode("utf8")
+            stderr = proc.stderr.decode("utf8")
+        
+        return render_template("code_test.html", code=code, stdin=stdin, stdout=stdout, stderr=stderr)
+
+if __name__ == "__main__":
+	app.run(host="localhost", port=5000)
 
 
