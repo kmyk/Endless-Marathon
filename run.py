a16159930b49c74e7c11c933defa1c81b0861c3e
kosakkun <kosakkun0918@gmail.com>
2019-02-26 20:21:50 +0900

    code_testのページをflaskで作成した．

--- run.py
+++ run.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import *
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
+@app.route('/code_test', methods=["GET", "POST"])
+def code_test():
+    if request.method == "GET":
+        return render_template('code_test.html')
+    else:
+        return render_template('code_test.html')
+    
 if __name__ == '__main__':
 	app.run()
 
