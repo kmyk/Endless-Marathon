656e7e3fa0d84fafc0d90ca6278e22da18faf783
kosakkun <kosakkun0918@gmail.com>
2019-02-27 01:28:01 +0900

    提出したファイルのコンパイルチェックとDBへの登録

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import os
 import subprocess
+import pymysql.cursors
+import time 
 
 from flask import *
 app = Flask(__name__)
@@ -35,10 +37,93 @@ def code_test():
         
         return render_template("code_test.html", code=code, stdin=stdin, stdout=stdout, stderr=stderr)
 
+    
 ##################################################################################################################
 @app.route("/submit", methods=["GET", "POST"])
 def submit():
-    return render_template("submit.html")
+    if request.method == "GET":
+        return render_template("submit.html", error="")
+    else:
+        lang = request.form["lang-sel"]
+        prob = request.form["prob-sel"]
+        user = request.form["userid"]
+        code = request.form["source_code"] 
+            
+        if user == "":
+            return render_template("submit.html", error="UserID is empty!")
+        if code == "":
+            return render_template("submit.html", error="Source Code is empty!")
+        
+        if lang == "cpp":
+            try:
+                if not os.path.exists("execute"):
+                    os.makedirs("execute")
+                with open("execute/a.cpp", "w") as file:
+                    file.write(code)
+                cmd = "g++ execute/a.cpp -std=c++11 -o execute/a.out"
+                ret = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
+            except subprocess.CalledProcessError as e:   
+                return render_template("submit.html", error="Compile Error! " + str(e.output))
+  
+        code_length = 0
+        time_stamp = time.strftime('%Y-%m-%d %H:%M:%S')
+        score = 0
+        execution_time = 0
+
+        ############
+        connection = pymysql.connect(
+            host        = "localhost",
+            user        = "root",
+            password    = "",
+            db          = "endless_marathon",
+            charset     = "utf8",
+            cursorclass = pymysql.cursors.DictCursor)
+
+        # submit id
+        code_id = ""
+        with connection.cursor() as cursor:   
+            sql = "select count(code_id) from submissions";
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            code_id = int(results[0]["count(code_id)"]) + 1
+        
+        with connection.cursor() as cursor:
+            sql = '''INSERT INTO submissions (
+                code_id, 
+                user_id, 
+                problem_id, 
+                language, 
+                code,
+                code_length,
+                execution_time,
+                score,
+                time_stamp) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+            r = cursor.execute(sql, (
+                code_id, 
+                user, 
+                prob, 
+                lang, 
+                code,
+                code_length,
+                execution_time,
+                score,
+                time_stamp))
+            connection.commit()
+        connection.close()
+
+        return '''
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                    <meta name="author" content="kosakkun">
+                    <meta name="description" content="">
+                    <title>Endless Marathon</title>
+                </head>
+                <body>
+                    Success! : <a href="/submit">Submit</a>
+                </body>
+            </html>'''
 
 ##################################################################################################################
 @app.route("/submissions", methods=["GET", "POST"])
